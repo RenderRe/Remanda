198f650e7ac7d6c7648eab4c9d50d1dd26315fb1
RenderRe <danila_boitsov@mail.ru>
2024-02-26 20:01:01 +0500

    Fix 8 bags

--- js/Login.js
+++ js/Login.js
@@ -4,7 +4,7 @@ function login() {
 
     // Пример простой проверки данных (можете заменить на более сложную проверку)
     if (username === 'Dren' && password === 'Prod') {
-        window.location.href = '/page.html'
+        window.location.href = 'page/page.html'
     } else if (username === 'user2' && password === 'password2') {
         alert('Login successful for User 2');
     } else {
